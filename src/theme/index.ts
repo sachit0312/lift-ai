6a1978f2afaaa8d1a17659602a78124d21d5f47a
Sachit Goyal <sachitgoyal6@gmail.com>
2026-05-17 20:14:12 -0700

    refactor(theme): delete dead cardStyles/buttonStyles/inputStyles/emptyStateStyles

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,4 @@
 export { colors, spacing, fontSize, fontWeight, borderRadius, layout } from './tokens';
-export { modalStyles, cardStyles, buttonStyles, inputStyles, emptyStateStyles } from './sharedStyles';
+export { modalStyles } from './sharedStyles';
 export { authStyles } from './authStyles';
 export { chipStyles } from './chipStyles';
